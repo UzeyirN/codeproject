fe06427126427bd8dbf7a363b4a7bfdd093aa512
UzeyirN <7qzjlm4@code.edu.az>
2023-02-08 00:11:31 +0400

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../styles/Footer.css'
 
 const Footer = () => {
     return (
-        <>Footer</>
+        <>
+            <div className="footer-wrapper">
+                <div className="footer-banners">
+                    <div className="footer-banner__left">
+                        <div class="card-bg"></div>
+                        <div class="card-text">Hover Over Me</div>
+                    </div>
+                    <div className="footer-banner__right">
+                        <div class="card2-bg"></div>
+                        <div class="card2-text">Hover Over Me</div>
+                    </div>
+                </div>
+                <div className="footer-content"></div>
+            </div>
+        </>
     )
 }
 
