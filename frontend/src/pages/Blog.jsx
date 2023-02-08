f1819c6499e67489d33acb1d0cc207712dfdf898
UzeyirN <7qzjlm4@code.edu.az>
2023-02-08 23:26:37 +0400

    blog

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,12 +1,61 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import '../styles/Blog.css'
+import { Link } from 'react-router-dom';
 
 const Blog = () => {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Blog</title>
-    </Helmet>
+      </Helmet>
+      <div className='blog-top'></div>
+      <div className="container">
+        <div className="blog-wrapper">
+          <div className='blog-head'>
+            <Link className='blog-link playfair-font' to='' >Wine Fermentation Process</Link>
+            <p className='lato-font' style={{ color: "RGB(176, 151, 109)" }}>9th Mar 2017</p>
+            <Link>
+              <img className='blog-img' style={{ width: "100%", height: "100%" }} src="https://cdn11.bigcommerce.com/s-qbep6rt4nh/images/stencil/9999x9999/uploaded_images/blog-post-2.jpg?t=1489066586" alt="" />
+            </Link>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus posuere mattis.
+              Suspendisse venenatis massa in mattis condimentum. Vivamus porta, tellus vel ultricies facilisis, dolor arcu grav …</p>
+
+            <Link style={{ color: "RGB(176, 151, 109)", textDecoration: "none" }}>read more</Link>
+          </div>
+        </div>
+
+        <div className="blog-wrapper">
+          <div className='blog-head'>
+            <Link className='blog-link playfair-font' to='' >When is a vineyard ready for harvest?</Link>
+            <p className='lato-font' style={{ color: "RGB(176, 151, 109)" }}>9th Mar 2017</p>
+            <Link>
+              <img className='blog-img' style={{ width: "100%", height: "100%" }} src="https://cdn11.bigcommerce.com/s-qbep6rt4nh/images/stencil/9999x9999/uploaded_images/blog-post-1.jpg?t=1489066679" alt="" />
+            </Link>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus posuere mattis.
+              Suspendisse venenatis massa in mattis condimentum. Vivamus porta, tellus vel ultricies facilisis, dolor arcu grav …</p>
+
+            <Link style={{ color: "RGB(176, 151, 109)", textDecoration: "none" }}>read more</Link>
+          </div>
+        </div>
+
+        <div className="blog-wrapper">
+          <div className='blog-head'>
+            <Link className='blog-link playfair-font' to='' >Winery of the year</Link>
+            <p className='lato-font' style={{ color: "RGB(176, 151, 109)" }}>15th Feb 2014</p>
+            <Link>
+              <img className='blog-img' style={{ width: "100%", height: "100%" }} src="https://cdn11.bigcommerce.com/s-qbep6rt4nh/images/stencil/9999x9999/uploaded_images/blog-post-3.jpg?t=1489064769" alt="" />
+            </Link>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus posuere mattis.
+              Suspendisse venenatis massa in mattis condimentum. Vivamus porta, tellus vel ultricies facilisis, dolor arcu grav …</p>
+
+            <Link style={{ color: "RGB(176, 151, 109)", textDecoration: "none" }}>read more</Link>
+          </div>
+        </div>
+
+       
+      </div>
+
     </>
   )
 }
