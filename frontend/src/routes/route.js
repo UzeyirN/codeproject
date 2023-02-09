5a1377fb88f9da2b95c874ad9140a3363cbd5ecf
UzeyirN <7qzjlm4@code.edu.az>
2023-02-09 18:47:43 +0400

    blog and its pages with responsivness

--- frontend/src/routes/route.js
+++ frontend/src/routes/route.js
@@ -1,13 +1,15 @@
 import MainRoot from '../components/MainRoot'
 import Home from './../pages/Home';
-import AllShopWines from './../pages/ShopWines/AllShopWines';
-import RedWines from './../pages/ShopWines/RedWines';
-import WhiteWines from './../pages/ShopWines/WhiteWines';
-import RoseWines from './../pages/ShopWines/RoseWines';
+import AllShopWines from '../pages/BlogPages/ShopWines/AllShopWines';
+import RedWines from '../pages/BlogPages/ShopWines/RedWines';
+import WhiteWines from '../pages/BlogPages/ShopWines/WhiteWines';
+import RoseWines from '../pages/BlogPages/ShopWines/RoseWines';
 import OurStory from './../pages/About/OurStory';
 import TheEstate from './../pages/About/TheEstate';
 import ShippingReturn from './../pages/About/ShippingReturn';
-import Blog from './../pages/Blog';
+// blog
+import BlogRoot from '../pages/BlogPages/BlogRoot';
+import Blog from '../pages/BlogPages/Blog';
 import ContactUs from './../pages/ContactUs';
 import Winery from './../pages/BlogPages/Winery';
 import WineFermantation from './../pages/BlogPages/WineFermantation';
@@ -52,26 +54,34 @@ const ROUTES = [
                 element: <ShippingReturn />
             },
             {
-                path: "blog",
-                element: <Blog />
+                path: "blog/",
+                element: <BlogRoot />,
+                children: [
+                    {
+                        path: "",
+                        element: <Blog />
+                    },
+                    {
+                        path: "winefermantation",
+                        element: <WineFermantation />
+                    },
+                    {
+                        path: "harvest",
+                        element: <Harvest />
+                    },
+                    {
+                        path: "winery",
+                        element: <Winery />
+                    }
+                    
+                ]
 
             },
             {
                 path: "contact",
                 element: <ContactUs />
-            },
-            {
-                path: "winery",
-                element: <Winery />
-            },
-            {
-                path: "winefermantation",
-                element: <WineFermantation />
-            },
-            {
-                path: "harvest",
-                element: <Harvest />
             }
+
 
 
         ]
