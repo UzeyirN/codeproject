314d2f0e005baf6382f3c1c3b1620f909d534bf8
UzeyirN <7qzjlm4@code.edu.az>
2023-03-01 14:31:55 +0400

    Gazamiz mubarek olsun

--- frontend/src/components/HomeSections/HeroSection.jsx
+++ frontend/src/components/HomeSections/HeroSection.jsx
@@ -26,18 +26,6 @@ const HeroSection = () => {
                     style={{ "--swiper-navigation-color": "b0976d", "--swiper-pagination-color": "#b0976d" }}
 
                 >
-                    <SwiperSlide className='slider1 slider'>
-                        <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src="https://cdn11.bigcommerce.com/s-qbep6rt4nh/images/stencil/original/carousel/15/slide-2-background__86858.jpg?c=2" alt="glass" />
-                        <div className=" hero-text">
-                            <div className="row">
-                                <div className=" hero-content">
-                                    <p className='lato-font herosec-p'>VILLENOIR WINES</p>
-                                    <h1 className='playfair-font herosec-h1'>Tradition in a glass</h1>
-                                    <button className='herosec-btn lato-font'><Link to='allshopwines' className='lato-font hero-link'>SHOP NOW</Link></button>
-                                </div>
-                            </div>
-                        </div>
-                    </SwiperSlide>
                     <SwiperSlide className='slider1'>
                         <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src="https://cdn11.bigcommerce.com/s-qbep6rt4nh/images/stencil/original/carousel/9/slide-1-background.jpg?c=2" alt="wine" />
                         <div className=" hero-text">
@@ -46,6 +34,18 @@ const HeroSection = () => {
                                     <p className='lato-font herosec-p'>VILLENOIR WINES</p>
                                     <h1 className='playfair-font herosec-h1'>Refinement in a bottle</h1>
                                     <button className='herosec-btn lato-font'><Link to='allshopwines' className='lato-font hero-link'>SHOP THE WINES</Link></button>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='slider1 slider'>
+                        <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src="https://cdn11.bigcommerce.com/s-qbep6rt4nh/images/stencil/original/carousel/15/slide-2-background__86858.jpg?c=2" alt="glass" />
+                        <div className=" hero-text">
+                            <div className="row">
+                                <div className=" hero-content">
+                                    <p className='lato-font herosec-p'>VILLENOIR WINES</p>
+                                    <h1 className='playfair-font herosec-h1'>Tradition in a glass</h1>
+                                    <button className='herosec-btn lato-font'><Link to='allshopwines' className='lato-font hero-link'>SHOP NOW</Link></button>
                                 </div>
                             </div>
                         </div>
